53250719ae713d43df43f34c3777e1e3b61eaf0b
Jason Arnold <jason.arnold.00@gmail.com>
2017-06-26 12:13:26 -0400

    fixing API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,44 @@
 import axios from 'axios';
+import keys from './keys';
 
 export const fetchSongs = (artistName) => {
+  const clientId = keys.client_id;
+  const clientSecret = keys.client_secret;
   const artistNameUrl = `https://api.spotify.com/v1/search?q=${artistName}&type=artist`;
-  const nameRequest = axios.get(artistNameUrl);
 
-  return (dispatch) => {
-    nameRequest.then(({ data }) => {
-      const artistId = data.artists.items[0].id;
-      const artistIdUrl = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=ES`;
-      const IdRequest = axios.get(artistIdUrl);
-
-      IdRequest.then(({ data }) => {
-        dispatch({
-          type: 'FETCH_SONGS',
-          payload: data
-        });
-      });
-    });
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
   };
+
+  axios.post(authOptions, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log('it worked', body);
+    }
+  });
+
+  // const nameRequest = axios.get(artistNameUrl);
+  //
+  // return (dispatch) => {
+  //   nameRequest.then(({ data }) => {
+  //     const artistId = data.artists.items[0].id;
+  //     const artistIdUrl = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=ES`;
+  //     const IdRequest = axios.get(artistIdUrl);
+  //
+  //     IdRequest.then(({ data }) => {
+  //       dispatch({
+  //         type: 'FETCH_SONGS',
+  //         payload: data
+  //       });
+  //     });
+  //   });
+  // };
 };
 
 export const setTerm = (term) => {
