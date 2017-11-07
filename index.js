7d4076c709926e0df939d54a9f4d0ab26ae73fee
Jason Arnold <jason.arnold.00@gmail.com>
2017-11-07 13:33:17 -0500

    change static path

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ require('./routes/authRoutes')(app);
 require('./routes/apiRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/public'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
