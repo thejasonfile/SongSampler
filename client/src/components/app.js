090c18c4b38997d60d74e0929103eeafdce62835
Jason Arnold <jason.arnold.00@gmail.com>
2017-11-07 15:58:52 -0500

    user data being put into state

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -9,9 +9,35 @@ import Landing from './landing';
 import NoArtist from './no_artist';
 
 class App extends Component {
-  componentWillMount(user) {
-    
+  componentWillMount() {
+    const user = this.props.getCurrentUser();
+    //this.renderRoutes(user);
   }
+  //
+  // renderRoutes(user) {
+  //   debugger;
+  //   if (!user) {
+  //     return (
+  //       <BrowserRouter>
+  //         <div>
+  //           <Route path="/" component={Landing} />
+  //         </div>
+  //       </BrowserRouter>
+  //     )
+  //   } else {
+  //     return (
+  //       <BrowserRouter>
+  //         <div>
+  //           <Route exact path="/" component={Landing} />
+  //           <Route path="/search" component={SearchBar} />
+  //           <Route path="/search/results" component={SongList} />
+  //           <Route path="/search/noartist" component={NoArtist} />
+  //         </div>
+  //       </BrowserRouter>
+  //     )
+  //   }
+  // }
+
   render() {
     return (
       <div>
@@ -29,7 +55,7 @@ class App extends Component {
 }
 
 function mapStateToProps(state) {
-  return { user: state.term };
+  return { user: state.user };
 }
 
 export default connect(mapStateToProps, { getCurrentUser })(App);
